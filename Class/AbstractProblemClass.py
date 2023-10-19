f55af6b3fac90c225da8487839620cbdc69011a0
ANTONIA BLANCO <antonia.blanco@uc.cl>
2023-10-19 18:30:46 -0300

    feat: se escribe la documentación de las clases publicas

--- Class/AbstractProblemClass.py
+++ Class/AbstractProblemClass.py
@@ -1,8 +1,28 @@
 from abc import ABC, abstractmethod
 
+
 class AbstractProblem(ABC):
+    '''
+    Esta clase ofrece una interfaz para que los usuarios puedan implementar sus 
+    propias clases Problem. Las cuales pueden utilizar con la clase proporcionada MDD 
+    para resolver diagramas de decisión.
+    '''
 
     def __init__(self, initial_state, variables):
+        '''
+        Constructor de la clase AbstractProblem.
+
+        Parámetros:
+        - initial_state (NOT DEFINE): El estado inicial del problema, puede ser una lista, int, 
+        string, etc. Según lo que se requiera para el problema.
+        - variables (list): Una lista que contiene las variables del problema y su dominio, 
+        deben seguir el formato List<List<VARIABLE, DOMINIO>> .
+
+        Atributos:
+        - initial_state (NOT DEFINE): El estado inicial del problema. 
+        - ordered_variables (list): Lista de variables ordenadas.
+        - variables_domain (dict): Diccionario que mapea variables a su dominio.
+        '''
         self.initial_state = initial_state
         self.ordered_variables = self.get_variables(variables)
         self.variables_domain = self.get_variables_domain(variables)
@@ -21,10 +41,33 @@ class AbstractProblem(ABC):
 
     @abstractmethod
     def equals(self, state_one, state_two):
+        '''
+        Método abstracto que debe ser implementado por las subclases para determinar si dos estados son iguales.
+
+        Parámetros:
+        state_one: El primer estado a comparar.
+        state_two: El segundo estado a comparar.
+
+        Retorna:
+        bool: True si los estados son iguales, False en caso contrario.
+        '''
         pass
 
     @abstractmethod
     def transition_function(self, previus_state, variable_id, variable_value):
+        '''
+        Método abstracto que debe ser implementado por las subclases para definir la función de transición.
+
+        Parámetros:
+        previus_state: El estado anterior.
+        variable_id: El identificador de la variable que se modifica, sigue el mismo type que el entregado en 
+        variables.
+        variable_value: El valor de la variable que se modifica, es el valor posible de su dominio que se selecciono.
+
+        Retorna:
+        tuple: Una tupla que contiene el nuevo estado, siguiente el tipo entregado en inicial_state y un 
+        flag, que es True si el estado es factible, y False en caso contrario.
+        '''
         pass
 
     
--- Class/MDD.py
+++ Class/MDD.py
@@ -4,15 +4,30 @@ from Class.Print import Print
 from Class.MargaritaFile import MargaritaFile
 from Class.MinMaxObjective import MinMaxFunction
 
-class MDD():
 
+class MDD():
+    '''
+    Clase MDD (Multi-Valued Decision Diagram) para la creación y manipulación de diagramas de decisión.
+    '''
     def __init__(self, problem):
+        '''
+        Constructor de la clase MDD.
+
+        Parámetros:
+        problem: Una instancia de la clase problem, que se utilizará para crear el diagrama de decisión.
+
+        Atributos:
+        - problem: Una instancia de la clase problem, que se utilizará para crear el diagrama de decisión.
+        - DD: El diagrama de decisión creado, que se actualiza al generar el diagrama reducido o relajado.
+        '''
         self.problem = problem
         self.DD = None
-        self.reduceDD = None
         self.create_decision_diagram()
 
     def create_decision_diagram(self):
+        '''
+        Crea el diagrama de decisión utilizando el problema proporcionado y lo guarda en self.DD.
+        '''
         print("")
         print("Iniciando la creación del diagrama de decision ...")
         self.constructor = Constructor(self.problem)
@@ -20,6 +35,9 @@ class MDD():
         print("Diagrama de decision creado")
     
     def create_reduce_decision_diagram(self):
+        '''
+        Reduce el diagrama de decisión y lo guarda en self.DD.
+        '''
         print("")
         print("Iniciando la reducción del diagrama de decision ...")
         self.reduce_constructor = ReduceConstructor(self.DD)
@@ -32,19 +50,40 @@ class MDD():
 
 
     def get_margarita_file(self, file_name):
+        '''
+        Obtiene un archivo Margarita para el diagrama de decisión.
+
+        Parámetros:
+        file_name (str): El nombre del archivo Margarita.
+
+        Retorna:
+        str: El nombre del archivo Margarita generado.
+        '''
         margarita_file = MargaritaFile(file_name, self.DD)
         return margarita_file.file_name
     
     def develop_solver(self, variable_ranges, function_operations, weights, objective):
+        '''
+        Guarda la información necesaria para tener una función objetivo.
+
+        Parámetros:
+        variable_ranges (list): Rangos de las variables del problema.
+        function_operations (list): Operaciones a aplicar en la función objetivo.
+        weights (list): Pesos para las variables del problema.
+        objective (str): Tipo de objetivo (por ejemplo, "min" o "max").
+        '''
         try:
             self.minmax = MinMaxFunction(variable_ranges, function_operations, weights, objective)
             self.minmax.assign_transition_values(self.reduceDD)
         except Exception as e:
             print(e)
             raise Exception("Solver not defined")
-
     
     def solve_dd(self):
+        '''
+        Resuelve el diagrama de decisión, obteniendo la mejor solución para la función objeitivo
+        entregada en develop_solver.
+        '''
         try:
             self.minmax.anti_dijkstra(self.reduceDD.structure[0][0])
         except Exception as e:
