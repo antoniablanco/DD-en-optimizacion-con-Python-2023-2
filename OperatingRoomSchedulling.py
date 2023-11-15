9b193235433266704849f3988aa4a377e688a229
Nicolás Casassus <ncasassus@uc.cl>
2023-11-15 13:23:36 -0300

    Implementación rápida de Operating Room Schedulling

--- OperatingRoomSchedulling.py
+++ OperatingRoomSchedulling.py
@@ -1,9 +1,10 @@
 from Class.AbstractProblemClass import AbstractProblem
 from Class.DD import DD
 from Class.ObjectiveFunction import ObjectiveFunction
+import numpy as np
 
 
-class ProblemKnapsack(AbstractProblem):
+class OperatingRoomSchedulling(AbstractProblem):
 
     def __init__(self, initial_state, variables):
         super().__init__(initial_state, variables)
@@ -15,17 +16,20 @@ class ProblemKnapsack(AbstractProblem):
         print(previus_state)
         print(variable_id)
         print(variable_value)
-        lista_suma_variables = [3, 3, 4, 6]
-        new_state = [int(previus_state[0])+lista_suma_variables[int(variable_id[2:])-1]*int(variable_value)]
+        is_feasible = True
+        if variable_value in previus_state:
+            is_feasible = False
+        new_state = [i for i in previus_state]
+        new_state.append(variable_value)
+        return new_state, is_feasible
 
-        isFeasible = int(new_state[0]) <= 6
-        return new_state, isFeasible
+number_operations = 4
 
+# El estado es operaciones hechas, tiempo usado y ultima operacion
+initial_state = []
+variables = [(f'x_{i}', list(range(number_operations))) for i in range(number_operations)]
 
-initial_state = [0]
-variables = [('x_1', [0, 1]), ('x_2', [0, 1]), ('x_3', [0, 1]), ('x_4', [0, 1])]
-
-problem_instance = ProblemKnapsack(initial_state, variables)
+problem_instance = OperatingRoomSchedulling(initial_state, variables)
 
 dd_instance = DD(problem_instance, v=False)
 
@@ -35,8 +39,3 @@ dd_instance.print_decision_diagram()
 dd_instance.export_graph_file("test")
 
 decision_diagram = dd_instance.get_decision_diagram_graph()
-
-objective_function_instance = ObjectiveFunction(decision_diagram)
-objective_function_instance.develop_solver([-5, 1, 18, 17], 'max')
-objective_function_instance.solve_dd()
-
