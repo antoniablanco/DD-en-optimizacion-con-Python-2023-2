04a072a43824f9577825d7b7ac3f059f7c331047
ANTONIA BLANCO <antonia.blanco@uc.cl>
2024-01-13 16:02:38 -0300

    fix: se cambian los test casses para la función objetivo

--- Class/DDBuilder/DDBuilder.py
+++
@@ -1,9 +0,0 @@
-from Class.DDBuilder.AbstractDDBuilder import AbstractDDBuilder
-
-class DDBuilder(AbstractDDBuilder):
-
-    def _specific_layer_function(self):
-        pass
-
-    def _specific_final_function(self):
-        pass
